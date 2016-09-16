be8f5e7bb1905e49509f9f3e2a65286f01b4b3d9
Njål nordmark <njaal.nordmark@gmail.com>
2016-09-16 22:16:41 +0200

    Minor restructuring
    Renamed main class and updated testfile to @Test-notation

--- intervalcalculator/src/main/java/com/njanor/intervalcalculator/IntervalCalculatorApplication.java
+++ intervalcalculator/src/main/java/com/njanor/intervalcalculator/IntervalCalculatorApplication.java
@@ -4,7 +4,7 @@ package com.njanor.intervalcalculator;
  * Hello world!
  *
  */
-public class App 
+public class IntervalCalculatorApplication
 {
     public static void main( String[] args )
     {
---
+++ intervalcalculator/src/test/java/com/njanor/intervalcalculator/JUnitTest.java
@@ -0,0 +1,18 @@
+package com.njanor.intervalcalculator;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class JUnitTest
+{
+    @Test
+    public void testShouldPass() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void testShouldFail() {
+        assertTrue(false);
+    }
+}
