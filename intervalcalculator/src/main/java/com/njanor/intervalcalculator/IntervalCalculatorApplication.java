cf83d886486204db0897e0f09c41075cd0d3bffd
Njål nordmark <njaal.nordmark@gmail.com>
2016-09-18 00:50:56 +0200

    Better input handling

--- intervalcalculator/src/main/java/com/njanor/intervalcalculator/IntervalCalculatorApplication.java
+++ intervalcalculator/src/main/java/com/njanor/intervalcalculator/IntervalCalculatorApplication.java
@@ -1,5 +1,6 @@
 package com.njanor.intervalcalculator;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class IntervalCalculatorApplication
@@ -9,9 +10,24 @@ public class IntervalCalculatorApplication
         List<Interval> intervalsToInclude = null;
         List<Interval> intervalsToExclude = null;
 
+        if (args.length == 0) {
+            System.out.println("Requires input of included intervals.");
+            System.out.println();
+            System.out.println("Example of input with only included intervals:");
+            System.out.println("20-30,50-40,10-45");
+            System.out.println();
+            System.out.println("Example of input with both included and excluded intervals:");
+            System.out.println("20-30,100-200 90-101,123-321");
+            return;
+        }
+
         try {
             intervalsToInclude = IntervalInterpreter.interpretIntervalsFromString(args[0]);
-            intervalsToExclude = IntervalInterpreter.interpretIntervalsFromString(args[1]);
+            if (args.length > 1) {
+                intervalsToExclude = IntervalInterpreter.interpretIntervalsFromString(args[1]);
+            } else {
+                intervalsToExclude = new ArrayList<>();
+            }
         } catch (IllegalArgumentException iae) {
             System.out.println("Illegal input");
             return;
