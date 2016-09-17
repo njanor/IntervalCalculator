4666fb1063c52c5439cd05e71a7fa04b6c7960cc
Njål nordmark <njaal.nordmark@gmail.com>
2016-09-17 22:19:45 +0200

    Correctly interprets input and outputs correct intervals

--- intervalcalculator/src/main/java/com/njanor/intervalcalculator/IntervalCalculatorApplication.java
+++ intervalcalculator/src/main/java/com/njanor/intervalcalculator/IntervalCalculatorApplication.java
@@ -1,13 +1,18 @@
 package com.njanor.intervalcalculator;
 
-/**
- * Hello world!
- *
- */
+import java.util.List;
+
 public class IntervalCalculatorApplication
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        List<ConcreteInterval> includeIntervals = IntervalInterpreter.interpretIntervals(args[0]);
+        List<ConcreteInterval> excludeIntervals = IntervalInterpreter.interpretIntervals(args[1]);
+
+        Intervals intervals = new Intervals();
+        includeIntervals.stream().forEach(ii -> intervals.includeInterval(ii.getLowerBound(), ii.getUpperBound()));
+        excludeIntervals.stream().forEach(ei -> intervals.excludeInterval(ei.getLowerBound(), ei.getUpperBound()));
+
+        System.out.println(intervals);
     }
 }
